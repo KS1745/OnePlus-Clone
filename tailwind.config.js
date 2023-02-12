912a5f8ae4b09e429014ab780112242960dbc1a3
KS1745 <kartikay.sinha17@gmail.com>
2023-02-13 02:38:38 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      black:"#000000",
+      white:"#ffffff",
+      Red:"#FF0000",
+      lightBlue:"#2b84ea",
+      
+    },
+  },
+  plugins: [],
+}
